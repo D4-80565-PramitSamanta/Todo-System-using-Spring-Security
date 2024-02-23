37605f38eda58c3d042bd8aa20bbbd5f87b9a3ca
D4-80565-PramitSamanta <63332503+D4-80565-PramitSamanta@users.noreply.github.com>
2024-02-24 02:13:26 +0530

    Authorization works without roles
    Authorization implemented
    without authentication

--- backend/src/main/java/com/app/SpringSecurityConfig.java
+++ backend/src/main/java/com/app/SpringSecurityConfig.java
@@ -1,13 +1,28 @@
 package com.app;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SpringSecurityConfig {
+	
+	@Bean
+	public static PasswordEncoder encoder()
+	{
+		return new BCryptPasswordEncoder();
+	}
+	
+	
 	@Bean
 	SecurityFilterChain securityFilterChain (HttpSecurity http ) throws Exception
 	{
@@ -16,5 +31,25 @@ public class SpringSecurityConfig {
 		{autherize.anyRequest().authenticated();
 	}).httpBasic(Customizer.withDefaults());
 		return http.build();
+	}
+	
+	@Bean
+	public UserDetailsService userDetailsService()
+	{
+		UserDetails ramesh = User.builder()
+				.username("ramesh")
+				.password(encoder().encode("111"))
+				.roles("USER")
+				.build();
+		
+		
+		UserDetails suresh = User.builder()
+				.username("suresh")
+				.password(encoder().encode("222"))
+				.roles("ADMIN")
+				.build();
+		
+		
+		return new InMemoryUserDetailsManager(ramesh, suresh);
 	}
 }
