7ca1285f306afe3dd74d122267c7bb234560cb34
D4-80565-PramitSamanta <63332503+D4-80565-PramitSamanta@users.noreply.github.com>
2024-02-24 13:04:53 +0530

    implemented spring security with mysql database
    i have defined customuserdetailservice to get username and roles from mysql database

--- backend/src/main/java/com/app/CustomUserDetailsService.java
+++ backend/src/main/java/com/app/CustomUserDetailsService.java
@@ -1,38 +1,39 @@
 package com.app;
 
-import java.util.Set;
-import java.util.stream.Collectors;
-
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
-import com.app.dao.UserDAO;
+import com.app.dao.UserRepository;
 import com.app.entities.User;
-import com.app.exception.ResourceNotFound;
 
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@Service
+@AllArgsConstructor
 public class CustomUserDetailsService implements UserDetailsService {
 
-	@Autowired
-	private UserDAO uDao;
-	
-	@Override
-	public UserDetails loadUserByUsername(String usernameoremail) throws UsernameNotFoundException {
-		 User user = uDao.findByUsernameOrEmail(usernameoremail, usernameoremail)
-				.orElseThrow(()->new ResourceNotFound("not found"));
-		 
-		 Set<SimpleGrantedAuthority> authorites = user.getRoles().stream()
-		 .map((role)->new SimpleGrantedAuthority(role.getName()))
-		 .collect(Collectors.toSet());
-		 
-		 return new org.springframework.security.core.userdetails.User(
-				 usernameoremail,
-				 user.getPassword(),
-				 authorites
-				 );
-				 
-	}
+    private UserRepository userRepository;
 
+    @Override
+    public UserDetails loadUserByUsername(String usernameOrEmail) throws UsernameNotFoundException {
+
+        User user = userRepository.findByUsernameOrEmail(usernameOrEmail, usernameOrEmail)
+                .orElseThrow(() -> new UsernameNotFoundException("User not exists by Username or Email"));
+
+        Set<GrantedAuthority> authorities = user.getRoles().stream()
+                .map((role) -> new SimpleGrantedAuthority(role.getName()))
+                .collect(Collectors.toSet());
+
+        return new org.springframework.security.core.userdetails.User(
+                usernameOrEmail,
+                user.getPassword(),
+                authorities
+        );
+    }
 }
